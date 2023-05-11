2e412038a342dd3a850ca2f57258b5d00d0187e6
jashsingh96 <jashdhaliwal96@gmail.com>
2023-05-11 09:19:21 -0500

    Removed capitalization and space from logo picture, updated burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 function menuToggle() {
   var x = document.getElementById("headerright");
+  if (x.className === 'header-right') {
   x.className += " responsive";
+} else {
+  x.className = 'header-right';
+}
 }
